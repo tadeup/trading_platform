724bc16f2ede39cdda6f99fd289822757140fba7
unknown <tadeup1@gmail.com>
2018-10-19 17:36:48 -0300

    active sideabar tab is now highlighted

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,11 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('dashboard', { layout: 'layout-dashboard' });
+    res.render('dashboard', {
+        layout: 'layout-dashboard',
+        active: {
+            'dashboard': true
+        }});
 });
 
-router.get('/test', function(req, res, next) {
-    res.render('dash-test', { layout: 'layout-dashboard' });
+router.get('/profile', function(req, res, next) {
+    res.render('profile', {
+        layout: 'layout-dashboard',
+        active: {
+            'profile': true
+        }});
+});
+
+router.get('/buy', function(req, res, next) {
+    res.render('buy', {
+        layout: 'layout-dashboard',
+        active: {
+            'buy': true
+        }});
+});
+
+router.get('/sell', function(req, res, next) {
+    res.render('sell', {
+        layout: 'layout-dashboard',
+        active: {
+            'sell': true
+        }});
+});
+
+router.get('/info', function(req, res, next) {
+    res.render('info', {
+        layout: 'layout-dashboard',
+        active: {
+            'info': true
+        }});
 });
 
 

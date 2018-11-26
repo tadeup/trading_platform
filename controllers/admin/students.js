2ad016fa5c1c9ada97f24c8b05e1df8c9c8b6947
unknown <tadeup1@gmail.com>
2018-11-25 22:52:38 -0200

    students tab initial implementation and few other tweeks

--- controllers/admin/students.js
+++ controllers/admin/students.js
@@ -1,12 +1,49 @@
-function studentsController(req, res, next) {
+const moment = require('moment');
+
+const {User} = require('../../models/Users');
+const {BuyOffer} = require('../../models/BuyOffers');
+const {SellOffer} = require('../../models/SellOffers');
+
+async function studentsController(req, res, next) {
+    const students = await User.find({isAdmin: false}).exec();
+    let offers;
+
+    if(req.query.student){
+        const {student} = req.query;
+        const studentBuyOffers = await BuyOffer.find({ownerId: student}).exec();
+        const studentSellOffers = await SellOffer.find({ownerId: student}).exec();
+
+        offers = studentBuyOffers
+            .map(a => {
+                return {
+                    asset: a.asset,
+                    q: a.buyQuantity,
+                    p: a.buyPrice,
+                    dateCreated : moment(a.dateCreated).format('YYYY-DD-MM HH:mm:ss'),
+                    dateCompleted : moment(a.dateCompleted).format('YYYY-DD-MM HH:mm:ss'),}
+                })
+            .concat(studentSellOffers
+                .map(a => {
+                    return {
+                        asset: a.asset,
+                        q: a.sellQuantity,
+                        p: a.sellPrice,
+                        dateCreated : moment(a.dateCreated).format('YYYY-DD-MM HH:mm:ss'),
+                        dateCompleted : a.dateCompleted
+                            ? moment(a.dateCompleted).format('YYYY-DD-MM HH:mm:ss')
+                            : "UNCOMPLETED"
+                    }
+                })
+            )
+    }
+
     let contextObj = {
         layout: 'admin',
-        // timesAndPrices,
-        positionOnAsset: req.user.assetPositions[`${assetName}`],
+        students,
+        offers,
         active: {
-            'prices': true,
+            'students': true,
         }};
-    contextObj.active[`${assetName}`] = true;
     res.render('admin/students', contextObj);
 }
 
